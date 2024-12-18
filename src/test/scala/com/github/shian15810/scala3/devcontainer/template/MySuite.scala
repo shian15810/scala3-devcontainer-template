054e859e6700a809789350dcb3a4e67bcf1a284b
Cheeshian Chuah <shian15810@gmail.com>
2024-12-18 10:20:37 +0000

    organize project layout into standard convention

--- src/test/scala/com/github/shian15810/scala3/devcontainer/template/MySuite.scala
+++ src/test/scala/com/github/shian15810/scala3/devcontainer/template/MySuite.scala
@@ -1,3 +1,5 @@
+import com.github.shian15810.scala3.devcontainer.template.hello
+
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
